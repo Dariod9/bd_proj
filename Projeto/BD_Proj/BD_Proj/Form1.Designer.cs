f27f7e52c57c1b930f976702daba15056ad9a9ab
Dário Matos <dario.matos@ua.pt>
2020-05-18 17:29:52 +0100

    Teste?

--- Projeto/BD_Proj/BD_Proj/Form1.Designer.cs
+++ Projeto/BD_Proj/BD_Proj/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace BD_Proj
+﻿using System.Windows.Forms;
+
+namespace BD_Proj
 {
     partial class Form1
     {
@@ -6,6 +8,32 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        private Panel panel1;
+        private SplitContainer splitContainer1;
+        private Panel panel2;
+        private ListBox listBox1;
+        private Panel panel3;
+        private TextBox txtID;
+
+        public Label Label11 { get; private set; }
+
+        private TextBox txtCountry;
+
+        public Label Label4 { get; private set; }
+        public Label Label10 { get; private set; }
+
+        private TextBox txtZIP;
+        private Button bttnOK;
+        private Button bttnEdit;
+
+        public Label Label9 { get; private set; }
+        public Label Label7 { get; private set; }
+        public Label Label6 { get; private set; }
+        public Label Label5 { get; private set; }
+        public Label Label3 { get; private set; }
+        public Label Label2 { get; private set; }
+        public Label Label1 { get; private set; }
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,10 +56,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtZIP = new System.Windows.Forms.TextBox();
+            this.bttnOK = new System.Windows.Forms.Button();
+            this.bttnEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 26);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1044, 535);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(150, 100);
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 96);
+            this.listBox1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(0, 0);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(0, 0);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 22);
+            this.txtCountry.TabIndex = 0;
+            // 
+            // txtZIP
+            // 
+            this.txtZIP.Location = new System.Drawing.Point(0, 0);
+            this.txtZIP.Name = "txtZIP";
+            this.txtZIP.Size = new System.Drawing.Size(100, 22);
+            this.txtZIP.TabIndex = 0;
+            // 
+            // bttnOK
+            // 
+            this.bttnOK.Location = new System.Drawing.Point(0, 0);
+            this.bttnOK.Name = "bttnOK";
+            this.bttnOK.Size = new System.Drawing.Size(75, 23);
+            this.bttnOK.TabIndex = 0;
+            // 
+            // bttnEdit
+            // 
+            this.bttnEdit.Location = new System.Drawing.Point(0, 0);
+            this.bttnEdit.Name = "bttnEdit";
+            this.bttnEdit.Size = new System.Drawing.Size(75, 23);
+            this.bttnEdit.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Projeto/BD_Proj/BD_Proj/Form1.cs
+++ Projeto/BD_Proj/BD_Proj/Form1.cs
@@ -16,5 +16,10 @@ namespace BD_Proj
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
